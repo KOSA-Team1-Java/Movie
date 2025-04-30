540fdca2d43d8bb8d3bb03a03a2ef006bedcdedb
jaeyong <ljy3573057@gmail.com>
2025-04-30 15:47:28 +0900

    edit Error Message

--- src/main/java/exception/ExceptionController.java
+++ src/main/java/exception/ExceptionController.java
@@ -2,18 +2,18 @@ package exception;
 
 public class ExceptionController {
     public void signUpError(Exception e) {
-        System.out.println("회원가입 오류 " + e.getMessage());
+        System.out.println("회원가입 오류 : " + e.getMessage());
     }
     
     public void loginError(Exception e) {
-        System.out.println("로그인 오류 " + e.getMessage());
+        System.out.println("로그인 오류 : " + e.getMessage());
     }
     
     public void reserveError(Exception e) {
-        System.out.println("예약 중 오류" + e.getMessage());
+        System.out.println("예약 중 오류 : " + e.getMessage());
     }
     
     public void paymentError(Exception e) {
-        System.out.println("결제 중 오류" + e.getMessage());
+        System.out.println("결제 중 오류 : " + e.getMessage());
     }
 }
