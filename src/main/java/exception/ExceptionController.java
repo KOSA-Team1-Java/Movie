b0487e5c06e80d5af46d870ddca458e119cbac53
jaeyong <ljy3573057@gmail.com>
2025-04-30 15:44:52 +0900

    add Error

--- src/main/java/exception/ExceptionController.java
+++ src/main/java/exception/ExceptionController.java
@@ -8,4 +8,12 @@ public class ExceptionController {
     public void loginError(Exception e) {
         System.out.println("로그인 오류 " + e.getMessage());
     }
+    
+    public void reserveError(Exception e) {
+        System.out.println("예약 중 오류" + e.getMessage());
+    }
+    
+    public void paymentError(Exception e) {
+        System.out.println("결제 중 오류" + e.getMessage());
+    }
 }
