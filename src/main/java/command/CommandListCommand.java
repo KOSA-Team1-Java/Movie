64114e7a90278f10dd59214dd776b0856939f04a
jei <(gks20877@naver.com>
2025-05-02 09:58:10 +0900

    update command

---
+++ src/main/java/command/CommandListCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+import controller.MainController;
+
+public class CommandListCommand implements Command {
+
+    @Override
+    public boolean execute(MainController context) {
+        System.out.println("사용 가능한 명령어 목록:");
+        System.out.println("/signup - 회원가입");
+        System.out.println("/login - 로그인");
+        System.out.println("/logout - 로그아웃");
+        System.out.println("/res - 영화 예매");
+        System.out.println("/exit - 종료");
+        return true;
+    }
+}
